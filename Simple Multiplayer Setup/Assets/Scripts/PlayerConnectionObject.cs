b339b773d054f1f21dc1d19e7bb6ee23faf37459
Michael Kidd <G00236920@gmit.ie>
2018-07-21 19:55:20 +0100

    Moving to Laptop

---
+++ Simple Multiplayer Setup/Assets/Scripts/PlayerConnectionObject.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnectionObject : NetworkBehaviour {
+
+    public GameObject PlayerUnitPrefab;
+
+    GameObject myPlayerUnit;
+
+    // Use this for initialization
+    void Start () {
+
+        if (!hasAuthority) {
+
+            return;
+        }
+
+        CmdSpawnMyUnit();
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+        if (!hasAuthority)
+        {
+
+            //This will prevent the command from affecting any object not owned by the client
+            //as it will end the method here
+            return;
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            CmdMoveUnitRight();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            CmdMoveUnitleft();
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            CmdMoveUnitUp();
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            CmdMoveUnitDown();
+        }
+
+    }
+
+    //Commands - only executed on the server
+    [Command]
+    void CmdSpawnMyUnit() {
+
+        //creates the object on the server
+        GameObject go = Instantiate(PlayerUnitPrefab);
+
+        myPlayerUnit = go;
+
+        //propagate the object to all clients
+        NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+
+    }
+
+
+    [Command]
+    void CmdMoveUnitUp()
+    {
+
+        this.transform.Translate(0, 0, 1);
+
+    }
+
+    [Command]
+    void CmdMoveUnitDown()
+    {
+
+        this.transform.Translate(0, 0, -1);
+
+    }
+
+    [Command]
+    void CmdMoveUnitleft()
+    {
+
+        this.transform.Translate(-1, 0, 0);
+
+    }
+
+    [Command]
+    void CmdMoveUnitRight()
+    {
+
+        this.transform.Translate(1, 0, 0);
+
+    }
+
+}
--- Simple Multiplayer Setup/Assets/Scripts/PlayerUnit.cs
+++ Simple Multiplayer Setup/Assets/Scripts/PlayerUnit.cs
@@ -13,33 +13,8 @@ public class PlayerUnit : NetworkBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (!isLocalPlayer) {
 
-            //This will prevent the command from affecting any object not owned by the client
-            //as it will end the method here
-            return;
-
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            this.transform.Translate(1, 0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            this.transform.Translate(-1, 0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            this.transform.Translate(0, 0, 1);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            this.transform.Translate(0, 0, -1);
-        }
 
     }
+    
 }
