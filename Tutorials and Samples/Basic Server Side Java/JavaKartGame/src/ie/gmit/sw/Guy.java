2e6a59dd04cde362e14cd8567216e0fa6305c281
Michael <G00236920@gmit.ie>
2018-10-03 00:29:18 +0000

    Updating Java program

---
+++ Tutorials and Samples/Basic Server Side Java/JavaKartGame/src/ie/gmit/sw/Guy.java
@@ -0,0 +1,12 @@
+package ie.gmit.sw;
+
+public class Guy {
+	
+	 private String name;
+	 
+	 public String getName() {
+		 
+	      return name;
+	      
+	  }     
+}
--- Tutorials and Samples/Basic Server Side Java/JavaKartGame/src/ie/gmit/sw/Server.java
+++ Tutorials and Samples/Basic Server Side Java/JavaKartGame/src/ie/gmit/sw/Server.java
@@ -1,42 +1,31 @@
 package ie.gmit.sw;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
  
 public class Server
 {
-
-    private static int portNumber = 5000;
- 
-    public static void main(String[] args)
-    {
-        try
-        {
-
-            System.out.println("Server Started and listening to the port 5000");
- 
-            while(true)
-            {
-                ServerSocket serverSocket = new ServerSocket(portNumber);
-                Socket clientSocket = serverSocket.accept();
-                
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                
-                System.out.println(out + " " + in);
-                
-            }
-            
-
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        
-    }
-}
+	 public static void main(String[] args)
+	 
+			 throws IOException {
+			      
+			      ServerSocket serverSocket = new ServerSocket(5000);
+			      
+			      System.out.println("Listening on port 5000");
+			      
+			      while (System.in.available() == 0) { 
+			    	  
+			    	  Socket socket = serverSocket.accept();
+			    	  
+			          System.out.println("A client has connected.");
+			          
+			          socket.close();
+			          
+			       }
+			      
+			      serverSocket.close();
+			      
+			  }
+			}
 
