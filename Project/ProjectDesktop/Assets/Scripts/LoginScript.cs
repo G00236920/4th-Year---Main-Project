f6272b841b685bfb71d735d65a51f7aa5b11ff6b
Michael Kidd <G00236920@gmit.ie>
2018-10-03 01:51:01 +0100

    Updating Java Server Program

--- Project/ProjectDesktop/Assets/Scripts/LoginScript.cs
+++ Project/ProjectDesktop/Assets/Scripts/LoginScript.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoginScript : MonoBehaviour {
 
@@ -15,18 +16,20 @@ public class LoginScript : MonoBehaviour {
     //Use port 5000
     private const int port = 5000;
     //Using UDP Client
-    private UdpClient client;
+    private UdpClient Client;
     //Use the Ip and port as End Point
-    private IPEndPoint ep;
+    private IPEndPoint Ep;
 
 
     public void ButtonClicked()
     {
 
+        Debug.Log("Connecting.....");
+
         try
         {
+
             TcpClient tcpclnt = new TcpClient();
-            Debug.Log("Connecting.....");
 
             tcpclnt.Connect("127.0.0.1", 5000);
             // use the ipaddress as in the server program
@@ -35,24 +38,14 @@ public class LoginScript : MonoBehaviour {
 
             Stream stm = tcpclnt.GetStream();
 
-            ASCIIEncoding message = new ASCIIEncoding();
-            byte[] ba = message.GetBytes("Hello from C#");
-            Debug.Log("Transmitting.....");
-
-            stm.Write(ba, 0, ba.Length);
-
-            byte[] bb = new byte[100];
-            int k = stm.Read(bb, 0, 100);
-
-            for (int i = 0; i < k; i++)
-                Debug.Log(Convert.ToChar(bb[i]));
+            SceneManager.LoadScene("Lobby", LoadSceneMode.Additive);
 
             tcpclnt.Close();
-        }
 
+        }
         catch (Exception)
         {
-            Debug.Log("Failed to Connect to Java");
+            Debug.Log("Failed to Connect to Server");
         }
 
     }
--- Tutorials and Samples/Basic Server Side Java/JavaKartGame/src/ie/gmit/sw/Server.java
+++ Tutorials and Samples/Basic Server Side Java/JavaKartGame/src/ie/gmit/sw/Server.java
@@ -27,5 +27,6 @@ public class Server
 			      serverSocket.close();
 			      
 			  }
+	 
 			}
 
