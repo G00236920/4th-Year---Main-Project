d3b16db998315ba99880a3d0f91ccc6b8b48553e
Michael Kidd <G00236920@gmit.ie>
2018-10-08 21:06:37 +0100

    Changed User Interfaces and added Scenes

---
+++ Project/ProjectDesktop/Assets/Scripts/ScoreboardScreen/ScoreboardScript.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreboardScript : MonoBehaviour {
+
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene("2.Lobby", LoadSceneMode.Additive);
+    }
+	
+}
