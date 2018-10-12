600c49bcaa3b6944e796e0a45da62ed2f6be4dc6
Michael Kidd <G00236920@gmit.ie>
2018-10-12 09:19:43 +0100

    Minor changes to buttons

--- Project/ProjectDesktop/Assets/Scripts/LoginScreen/LoginScript.cs
+++ Project/ProjectDesktop/Assets/Scripts/LoginScreen/LoginScript.cs
@@ -11,8 +11,6 @@ using UnityEngine.UI;
 
 public class LoginScript : MonoBehaviour {
 
-    //Use port 5000
-    private const int port = 5000;
     //Using UDP Client
     private UdpClient Client;
     //Use the Ip and port as End Point
@@ -29,8 +27,6 @@ public class LoginScript : MonoBehaviour {
 
             tcpclnt.Connect("52.18.149.174", 5000);
             // use the ipaddress as in the server program
-
-            Byte[] bytes = new Byte[1024];
               
             NetworkStream stream = tcpclnt.GetStream();
 
---
+++ Project/ProjectDesktop/Assets/Scripts/MatchMakingScreen/MatchMakingScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MatchMakingScript : MonoBehaviour {
+    const int PORT_NO = 5000;
+    const string SERVER_IP = "52.18.149.174";
+
+    public void ButtonClicked()
+    {
+
+        //---create a TCPClient object at the IP and port no.---
+            TcpClient client = new TcpClient();
+
+            client.Connect("52.18.149.174", 5001);
+            // use the ipaddress as in the server program
+
+        NetworkStream stream = client.GetStream();
+        
+        byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes("Hi from Unity");
+
+        stream.Write(bytesToSend, 0, bytesToSend.Length);
+
+        stream.Close();
+        client.Close();
+
+    }
+
+}
