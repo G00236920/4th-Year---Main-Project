1fc5bb6fe90b3ab30191c024d3e587d52057fcb2
G00306474 <G00306474@gmit.ie>
2018-10-28 13:35:05 +0000

    returns players sorted by score

--- Tutorials and Samples/BasicServerSidePython/MariaDBConnection.py
+++ Tutorials and Samples/BasicServerSidePython/MariaDBConnection.py
@@ -4,14 +4,33 @@ import mysql.connector
 con = mysql.connector.connect(port=5006,user='root',password='password',host='localhost',database='pythontest')
 
 cursor = con.cursor()
-query = ("SELECT id FROM test")
+query = ("SELECT playerName , score FROM test")
 
 cursor.execute(query)
 
+print("")
+print("Unformatted")
+print("")
+for (playerName,score) in cursor:
+      print("{},{}".format(
+   playerName,score))
 
-for (id) in cursor:
-      print("{}".format(
-   id))
+
+query1 = ("SELECT DENSE_RANK() OVER (ORDER BY score DESC) AS dense_rank, playerName, score FROM test ORDER BY score DESC;")
+
+cursor.execute(query1)
+
+print("")
+print("Sorted")
+print("WR| PlayerName  |Score")
+print("")
+
+for ( dense_rank,playerName,score) in cursor:
+      print("{} | {}  |{}".format(
+   dense_rank,playerName,score))
+
+
+
 
 cursor.close()
 con.close()
