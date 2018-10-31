7d99f5f914a93d4fa45a1d016753277d732e659a
G00306474 <G00306474@gmit.ie>
2018-10-31 14:08:34 +0000

    added xml reader for port 5004

--- Tutorials and Samples/BasicServerSidePython/MariaDBConnection.py
+++ Tutorials and Samples/BasicServerSidePython/MariaDBConnection.py
@@ -1,6 +1,29 @@
-
 import mysql.connector
+import socket
+import xml.etree.cElementTree as etree
+from io.StringIO import StringIO
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "localhost"
+port = "5004"
+addr = (host,port)
+s.connect(addr)
+
+def iparse(packet):
+    for _, element in etree.iterparse(packet):
+        print ("%s, %s" %(element.tag, element.text))
+        element.clear()
+    #if complete <event> node received, publish node
+
+data = "<feeds>"
+while 1:
+    chunk = s.recv(1024)
+    #replace the xml doc declarations as comments
+    data += (chunk.replace("<?","<!--")).replace("?>","-->")
+    iparse(StringIO(data))
+
+
+#MariaDB Connection
 con = mysql.connector.connect(port=5006,user='root',password='password',host='localhost',database='pythontest')
 
 cursor = con.cursor()
@@ -28,6 +51,7 @@ print("")
 for ( dense_rank,playerName,score) in cursor:
       print("{} | {}  |{}".format(
    dense_rank,playerName,score))
+
 
 
 
