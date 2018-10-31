579e00513c686756e028d2e3402e672a69095813
johnmannion51 <johnmannion51@gmail.com>
2018-10-31 16:33:55 +0000

    trying to verify users

--- Tutorials and Samples/Basic Server Side Java/JavaKartGame/src/ie/gmit/sw/VerifyLogin.java
+++ Tutorials and Samples/Basic Server Side Java/JavaKartGame/src/ie/gmit/sw/VerifyLogin.java
@@ -3,9 +3,17 @@ package ie.gmit.sw;
 import org.bson.Document;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import com.mongodb.client.model.Filters;
 
 public class VerifyLogin implements DatabaseConnection{
 
@@ -18,12 +26,16 @@ public class VerifyLogin implements DatabaseConnection{
 		//returns true if the user is in the mongoDB
 		MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
         
-        MongoDatabase database = mongoClient.getDatabase("usersdb");
+		MongoDatabase database = mongoClient.getDatabase("usersdb");
         
-        
-        BasicDBObject searchQuery = new BasicDBObject();
         MongoCollection<Document> collection = database.getCollection("users");
+        Document myDoc = collection.find(Filters.eq("uname", username)).first();
         
+        //System.out.println(myDoc);
+        if(myDoc == null) 
+        {
+        	return false;
+        }
 		return true;
 	}
 	
