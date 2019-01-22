9666b1c2268ec39c4f03a35d7442c4f68262ec66
Michael Kidd <G00236920@gmit.ie>
2019-01-22 10:43:10 +0000

    Added java dependencies to the lib folder in java

--- Tutorials and Samples/Basic Server Side Java/JavaKartGame/src/ie/gmit/sw/VerifyLogin.java
+++ Tutorials and Samples/Basic Server Side Java/JavaKartGame/src/ie/gmit/sw/VerifyLogin.java
@@ -2,17 +2,9 @@ package ie.gmit.sw;
 
 import org.bson.Document;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.Cursor;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoIterable;
 import com.mongodb.client.model.Filters;
 
 public class VerifyLogin implements DatabaseConnection{
@@ -24,6 +16,7 @@ public class VerifyLogin implements DatabaseConnection{
 	
 	public boolean findUserName(String username) {
 		//returns true if the user is in the mongoDB
+		@SuppressWarnings("resource")
 		MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
         
 		MongoDatabase database = mongoClient.getDatabase("usersdb");
