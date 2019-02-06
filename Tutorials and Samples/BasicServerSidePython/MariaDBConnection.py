369a1df2a9e1f3faecd54b46cc2fe4b99bc66bae
G00306474 <G00306474@gmit.ie>
2019-02-06 02:21:37 +0000

    added writing results from xml file to DB

--- Tutorials and Samples/BasicServerSidePython/MariaDBConnection.py
+++ Tutorials and Samples/BasicServerSidePython/MariaDBConnection.py
@@ -2,10 +2,8 @@
 import mysql.connector
 import socket
 import xml.etree.cElementTree as etree
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
+
+from io import StringIO
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = "localhost"
@@ -18,6 +16,9 @@ def main():
 def readXML():
     from xml.dom import minidom
 
+    playerNames = []
+    playerScore = []
+   
     # parse an xml file by name
     mydoc = minidom.parse('items.xml')
 
@@ -28,12 +29,43 @@ def readXML():
     print('\nAll Player names:')  
     for elem in results:  
         print(elem.attributes['name'].value)
-
+        playerNames.append(elem.attributes['name'].value)
+       
 
     # all Players results 
     print('\nAll Players results:')  
     for elem in results:  
         print(elem.firstChild.data)
+        playerScore.append(elem.firstChild.data)
+        
+
+    print(playerNames ,playerScore )
+    writeToDB(playerNames,playerScore)
+
+def writeToDB(playerNames,playerScore):
+    from mysql.connector.cursor import MySQLCursorPrepared
+    print(playerNames ,playerScore )
+    try:
+        #MariaDB Connection
+        con = mysql.connector.connect(port=5006,user='root',password='password',host='localhost',database='pythontest')
+        
+        records_to_insert = [ (playerNames[0],playerScore[0]) ,
+                            (playerNames[1],playerScore[1])
+                            ]
+        sql_insert_query = " INSERT INTO test (playerName, score) VALUES (%s,%s) "
+        myCursor = con.cursor()
+        print("Error Checking 1")
+        #used executemany to insert 3 rows
+        myCursor.executemany(sql_insert_query, records_to_insert)
+        print("Error Checking 2")
+        con.commit()
+        
+        print (myCursor.rowcount, "Record inserted successfully into python_users table")
+    except mysql.connector.Error as error :
+        print("Failed inserting record into python_users table {}".format(error))
+    myCursor.close()
+    con.close()
+
 
 
 def mariaConn():
