8435f6e7c2dab9e717d8b2705f36afc585cc056a
jono mannion <johnmannion51@gmail.com>
2019-02-07 16:04:38 +0000

    added server login folder to project

---
+++ Project/Server-Login/JavaKartGame/src/ie/gmit/sw/User.java
@@ -0,0 +1,35 @@
+package ie.gmit.sw;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String username;
+	private String password;
+	
+	User(String name, String pass) {
+		this.username = name;
+		this.password = pass;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ Project/Server-Login/JavaKartGame/src/ie/gmit/sw/VerifyLogin.java
@@ -0,0 +1,75 @@
+package ie.gmit.sw;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
+public class VerifyLogin implements DatabaseConnection{
+
+	public void add(String user) {
+		
+		MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
+        
+		MongoDatabase database = mongoClient.getDatabase("usersdb");
+        
+        MongoCollection<Document> collection = database.getCollection("users");
+        
+        Document doc = Document.parse( user );
+        
+        collection.insertOne(doc);
+        
+        System.out.println("New User Added to Database");
+        
+        mongoClient.close();
+	}
+	
+	public boolean findUserName(String username) {
+		//returns true if the user is in the mongoDB
+		MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
+        
+		MongoDatabase database = mongoClient.getDatabase("usersdb");
+        
+        MongoCollection<Document> collection = database.getCollection("users");
+        Document myDoc = collection.find(Filters.eq("uname", username)).first();
+        
+        mongoClient.close();
+        
+        //If the User name is not in the Database
+        if(myDoc == null)
+        {
+        	return false;
+        }
+        
+		return true;
+	}
+	
+	public boolean verifyPassword(String username, String password) {
+		
+		//returns true if the user is in the mongoDB
+		MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
+        
+		MongoDatabase database = mongoClient.getDatabase("usersdb");
+        
+        MongoCollection<Document> collection = database.getCollection("users");
+        Document myDoc = collection.find(Filters.eq("uname", username)).first();
+        
+        mongoClient.close();
+        
+        //If the User name is not in the Database
+        if(myDoc == null)
+        {
+        	return false;
+        }
+        else if(myDoc.containsValue(password)) {
+        	return true;
+        }
+        else {
+        	return false;
+        }
+
+	}
+	
+}
