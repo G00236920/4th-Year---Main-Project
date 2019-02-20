03a5c8b43baa5243d40db91a465adbbd86caca30
Michael Kidd <G00236920@gmit.ie>
2019-02-20 12:00:48 +0000

    Create a way to show a full list of servers on canvas

---
+++ Project/ProjectDesktop/Assets/Scripts/MatchMakingScreen/ServerGUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerGUI : MonoBehaviour {
+
+    public GameObject MainPanel;
+    public GameObject ServerPanel;
+
+	public void CancelClicked()
+    {
+		ServerPanel.gameObject.SetActive (false);
+        MainPanel.gameObject.SetActive (true);
+	}
+
+	public void RefreshClicked(){
+		
+	}
+    
+}
---
+++ Project/ProjectDesktop/Assets/Scripts/MatchMakingScreen/ServerList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerList : MonoBehaviour {
+
+	public GameObject MainPanel;
+    public GameObject ServerPanel;
+	private static ServerList _instance;
+    public static ServerList Instance { get { return _instance; } }
+	public List <Server> listOfServers = new List<Server>();
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+
+	public List<Server> getList(){
+		return listOfServers;
+	}
+
+	public void setList(List<Server> list){
+		listOfServers = list;
+	}
+
+}
