c7336a5e76c03835cbe55415b633bb2c85bfc67f
Michael Kidd <G00236920@gmit.ie>
2019-02-21 11:07:54 +0000

    Client server connection to python working

--- Project/ProjectDesktop/Assets/Scripts/ScoreboardScreen/ScoreboardScript.cs
+++ Project/ProjectDesktop/Assets/Scripts/ScoreboardScreen/ScoreboardScript.cs
@@ -9,46 +9,40 @@ using System.Text;
 using System.Xml;
 using System.Xml.Linq;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class ScoreboardScript : MonoBehaviour
-{
-    const int PORT_NO1 = 5004;
-    const int PORT_NO2 = 5005;
+public class ScoreboardScript : MonoBehaviour {
+    const int PORT_NO1 = 5005;
+    const int PORT_NO2 = 5006;
 
     private IPAddress SERVER_IP = IPAddress.Parse("52.18.149.174");// ip of vm
-    //private IPAddress SERVER_IP = IPAddress.Parse("192.168.0.103"); 
+    //private IPAddress SERVER_IP = IPAddress.Parse("127.0.0.1"); 
 
-
-    public void ButtonClicked()
+  public void ButtonClicked()
     {
-        CreateXml();// works creates xml file
-
         // Connect to a remote device.  
         try
         {
             // Establish the remote endpoint for the socket.  
             // The name of the    
-            IPEndPoint serverAddress = new IPEndPoint(SERVER_IP, PORT_NO2);// ip of vm on port 5005
+            IPEndPoint serverAddress = new IPEndPoint(SERVER_IP, PORT_NO1);
 
             // Create a TCP/IP socket.  
             Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             // Connect to the remote endpoint.  
             client.BeginConnect(serverAddress, new AsyncCallback(ConnectCallback), client);
-
+            
             Debug.Log("Connected");
 
             // Release the socket.  
             client.Shutdown(SocketShutdown.Both);
             client.Close();
-        }// try
+        }
         catch (Exception e)
         {
             Console.WriteLine(e.ToString());
-        }// catch
-
-    } //ButtonClicked 
+        }
+    } 
 
     private static void ConnectCallback(IAsyncResult ar)
     {
@@ -60,53 +54,56 @@ public class ScoreboardScript : MonoBehaviour
             // Complete the connection.  
             client.EndConnect(ar);
 
-            Debug.Log("Connected");
+             Debug.Log("Connected");
 
             Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint.ToString());
 
-        }// try
+        }
         catch (Exception e)
         {
             Console.WriteLine(e.ToString());
-        }// catch
-    }// ConnectCallback
+        }
+    }
+
+}
 
 
 
+/*
+private static void CreateXml()
+{
+    var users = new List<Users>() {
+    new Users() { Username = "Ray", Score = 10 },
+    new Users() { Username = "John", Score = 20 },
+    new Users() { Username = "Mike", Score = 30},
+    new Users() { Username = "Flan", Score = 200},
+    new Users() { Username = "Kevin", Score = 40}
+    };// player
 
-    private static void CreateXml()
-    {
-        var users = new List<Users>() {
-        new Users() { Username = "Ray", Score = 10 },
-        new Users() { Username = "John", Score = 20 },
-        new Users() { Username = "Mike", Score = 30},
-        new Users() { Username = "Flan", Score = 200},
-        new Users() { Username = "Kevin", Score = 40}
-        };// player
+    // SceneManager.LoadScene("2.Lobby", LoadSceneMode.Single);
+    XDocument xdoc = new XDocument(
+        new XDeclaration("1.0", "utf-8", "yes"),
+            // This is the root of the document
+            new XElement("Scores",
+            from usr in users
+            select
+                new XElement("Scores", new XAttribute("UserName", usr.Username),
+                new XElement("Score", usr.Score)
 
-        // SceneManager.LoadScene("2.Lobby", LoadSceneMode.Single);
-        XDocument xdoc = new XDocument(
-            new XDeclaration("1.0", "utf-8", "yes"),
-                // This is the root of the document
-                new XElement("Scores",
-                from usr in users
-                select
-                    new XElement("Scores", new XAttribute("UserName", usr.Username),
-                    new XElement("Score", usr.Score)
+                )));
 
-                    )));
+    xdoc.Save("Scores.xml"); // creates file in project/desktopProject
 
-        xdoc.Save("Scores.xml"); // creates file in project/desktopProject
-
-    }// CreateXml
+}// CreateXml
 
 }// ScoreboardScript
 
 
 public class Users
 {
-    public string Username { get; set; }
+public string Username { get; set; }
 
-    public int Score { get; set; }
+public int Score { get; set; }
 
 } // Users
+*/
