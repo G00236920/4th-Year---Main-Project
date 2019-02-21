6994c45b33a504bef5eacf7af2d0f309423bb99d
rayman51 <ray.mannionit2@gmail.com>
2019-02-21 17:04:38 +0000

    sped up connection able to send string to server

--- Project/ProjectDesktop/Assets/Scripts/ScoreboardScreen/ScoreboardScript.cs
+++ Project/ProjectDesktop/Assets/Scripts/ScoreboardScreen/ScoreboardScript.cs
@@ -20,6 +20,7 @@ public class ScoreboardScript : MonoBehaviour
 
     public void ButtonClicked()
     {
+        SendTest();
         List<Users> users = new List<Users>() {
     new Users() { Username = "Ray", Score = 10 },
     new Users() { Username = "John", Score = 20 },
@@ -27,83 +28,26 @@ public class ScoreboardScript : MonoBehaviour
     new Users() { Username = "Flan", Score = 200},
     new Users() { Username = "Kevin", Score = 40}
     };// Users
-        // Connect to a remote device.  
-        try
-        {
-            // Establish the remote endpoint for the socket.  
-            // The name of the    
-            IPEndPoint serverAddress = new IPEndPoint(SERVER_IP, PORT_NO1);
-
-            // Create a TCP/IP socket.  
-            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            // Connect to the remote endpoint.  
-            client.BeginConnect(serverAddress, new AsyncCallback(ConnectCallback), client);
-
-            Debug.Log("Connected 1");
-            Debug.Log(users);
-            //SendMessage(this.objectToByteArray(users));
-            TestSend ts = new TestSend();
-            // Release the socket.  
-            client.Shutdown(SocketShutdown.Both);
-            client.Close();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.ToString());
-        }
+        
     }
-
-
-    
-
-        private static void ConnectCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the socket from the state object.  
-                Socket client = (Socket)ar.AsyncState;
-
-                // Complete the connection.  
-                client.EndConnect(ar);
-
-                Debug.Log("Connected 2");
-
-                Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint.ToString());
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-
-    }
-
-    public class Users
+    public static void SendTest()
     {
-        public string Username { get; set; }
 
-        public int Score { get; set; }
+        String SERVER_IP = "52.18.149.174";
+        Int32 Port = 5005;
 
-    }
-public class TestSend
-{
+        Debug.Log("Connected 3");
+        // Use this for initialization
 
-    String Host = "52.18.149.174";
-    Int32 Port = 5005;
-
-    // Use this for initialization
-    void Start()
-    {
-        TcpClient tcpClient = new TcpClient(Host, Port);
+        Debug.Log("Connected 4");
+        TcpClient tcpClient = new TcpClient(SERVER_IP, Port);
 
         // Uses the GetStream public method to return the NetworkStream.
         NetworkStream netStream = tcpClient.GetStream();
 
         if (netStream.CanWrite)
         {
-            Byte[] sendBytes = Encoding.UTF8.GetBytes("Is anybody there?");
+            Byte[] sendBytes = Encoding.UTF8.GetBytes("Ahoy there Kevin");
             netStream.Write(sendBytes, 0, sendBytes.Length);
         }
         else
@@ -116,67 +60,54 @@ public class TestSend
             return;
         }
 
-        if (netStream.CanRead)
-        {
-            // Reads NetworkStream into a byte buffer.
-            byte[] bytes = new byte[tcpClient.ReceiveBufferSize];
 
-            // Read can return anything from 0 to numBytesToRead.
-            // This method blocks until at least one byte is read.
-            netStream.Read(bytes, 0, (int)tcpClient.ReceiveBufferSize);
 
-            // Returns the data received from the host to the console.
-            string returndata = Encoding.UTF8.GetString(bytes);
-
-            Debug.Log("This is what the host returned to you: " + returndata);
-
-        }
-        else
-        {
-            Debug.Log("You cannot read data from this stream.");
-            tcpClient.Close();
-
-            // Closing the tcpClient instance does not close the network stream.
-            netStream.Close();
-            return;
-        }
-        netStream.Close();
-    }
+    }//here
 }
-    /*var users = new List<Users>() {
-    new Users() { Username = "Ray", Score = 10 },
-    new Users() { Username = "John", Score = 20 },
-    new Users() { Username = "Mike", Score = 30},
-    new Users() { Username = "Flan", Score = 200},
-    new Users() { Username = "Kevin", Score = 40}
-    };// player
+
+public class Users
+{
+    public string Username { get; set; }
+
+    public int Score { get; set; }
+
+}
+
+
+/*var users = new List<Users>() {
+new Users() { Username = "Ray", Score = 10 },
+new Users() { Username = "John", Score = 20 },
+new Users() { Username = "Mike", Score = 30},
+new Users() { Username = "Flan", Score = 200},
+new Users() { Username = "Kevin", Score = 40}
+};// player
 /*
 private static string SendScores()
 {
-    var users = new List<Users>() {
-    new Users() { Username = "Ray", Score = 10 },
-    new Users() { Username = "John", Score = 20 },
-    new Users() { Username = "Mike", Score = 30},
-    new Users() { Username = "Flan", Score = 200},
-    new Users() { Username = "Kevin", Score = 40}
-    };// player
+var users = new List<Users>() {
+new Users() { Username = "Ray", Score = 10 },
+new Users() { Username = "John", Score = 20 },
+new Users() { Username = "Mike", Score = 30},
+new Users() { Username = "Flan", Score = 200},
+new Users() { Username = "Kevin", Score = 40}
+};// player
 
-   // SceneManager.LoadScene("2.Lobby", LoadSceneMode.Single);
-    XDocument xdoc = new XDocument(
-        new XDeclaration("1.0", "utf-8", "yes"),
-            // This is the root of the document
-            new XElement("Scores",
-            from usr in users
-            select
-                new XElement("Scores", new XAttribute("UserName", usr.Username),
-                new XElement("Score", usr.Score)
+// SceneManager.LoadScene("2.Lobby", LoadSceneMode.Single);
+XDocument xdoc = new XDocument(
+    new XDeclaration("1.0", "utf-8", "yes"),
+        // This is the root of the document
+        new XElement("Scores",
+        from usr in users
+        select
+            new XElement("Scores", new XAttribute("UserName", usr.Username),
+            new XElement("Score", usr.Score)
 
-                )));
+            )));
 
-    //string xml = xdoc.ToString();
+//string xml = xdoc.ToString();
 
-    //xdoc.Save("Scores.xml"); // creates file in project/desktopProject
-    return ("0");
+//xdoc.Save("Scores.xml"); // creates file in project/desktopProject
+return ("0");
 }// SendScores
 
 }// ScoreboardScript
