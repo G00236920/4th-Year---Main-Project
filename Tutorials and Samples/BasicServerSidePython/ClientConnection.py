806ac5c0529a80c0b079c81a5af23e42a8b32bc9
Kevin <G00306474@gmit.ie>
2019-02-21 10:58:50 +0000

    Multithreaded Python server connection created

--- Tutorials and Samples/BasicServerSidePython/ClientConnection.py
+++ Tutorials and Samples/BasicServerSidePython/ClientConnection.py
@@ -2,6 +2,8 @@
 import socketserver
 import socket
 
+from threading import Thread
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
@@ -10,8 +12,19 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         print (self.data)
         self.request.sendall(self.data.upper())
 
-if __name__ == "__main__":
-    HOST, PORT = '127.0.0.1', 5005
-
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+def listenToPort1():   
+    print("Listening on 5005.......")
+    server = socketserver.TCPServer((HOST1, PORT1), MyTCPHandler)
     server.serve_forever()
+
+def listenToPort2():   
+    print("Listening on 5006.......")
+    server = socketserver.TCPServer((HOST2, PORT2), MyTCPHandler)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    HOST1, PORT1 = '0.0.0.0', 5005
+    HOST2, PORT2 = '0.0.0.0', 5006
+
+    Thread( target = listenToPort1 ).start()
+    Thread( target = listenToPort2 ).start()
