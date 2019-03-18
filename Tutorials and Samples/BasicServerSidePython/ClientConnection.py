1ff08cceead2b1d241c4e1e3517557e0cfa1f70d
Kevin <G00306474@gmit.ie>
2019-03-18 19:17:00 +0000

    added comments to ClientConnection.py

--- Tutorials and Samples/BasicServerSidePython/ClientConnection.py
+++ Tutorials and Samples/BasicServerSidePython/ClientConnection.py
@@ -14,32 +14,30 @@ from threading import Thread
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("{} wrote:".format(self.client_address[0]))
-        currentTime()
-        testReturn = self.data 
-        xmlBytes = self.data.decode("utf-8")
-        xmlString = str( xmlBytes )
-        tree = ET.fromstring(xmlString)
+        self.data = self.request.recv(1024).strip()# recieves data from c# program
+        print ("{} wrote:".format(self.client_address[0]))# prints ip recieved from 
+        currentTime() # gets current time 
+        xmlBytes = self.data.decode("utf-8")# decodes bytes
+        xmlString = str( xmlBytes )#coverts bytes to string 
+        tree = ET.fromstring(xmlString)#creates xml from xmlString
         
-        Maria.readXML(tree)
-        returnXML = Maria.returnDef()
+        Maria.readXML(tree)#calls readXml def in MariaDBConnection.py and passes tree into it 
+        returnedXML = Maria.returnDef()#calls returnDef def in MariaDBConnection.py and stores return in returnedXML
 
-        self.request.sendall(returnXML)
-        print(returnXML)
+        self.request.sendall(returnedXML)#returns xml to c# program 
 
-def currentTime():
+def currentTime():#sets and prints current time 
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     print (st)
     print ("________________________________________________")
 
-def listenToPort1():   
+def listenToPort1():#sets up thread to listen on localhost & port 5005 and listen forever   
     print("Listening on 5005.......")
     server = socketserver.TCPServer((HOST1, PORT1), MyTCPHandler)
     server.serve_forever()
 
-def listenToPort2():   
+def listenToPort2():#sets up thread to listen on localhost & port 5006 and listen forever    
     print("Listening on 5006.......")
     server = socketserver.TCPServer((HOST2, PORT2), MyTCPHandler)
     server.serve_forever()
